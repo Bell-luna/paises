9a8c14b3f71165f6213a43c6ab4cba75c8412d8e
belen luna <barbarabluna8@gmail.com>
2023-02-08 15:19:32 -0300

    tercer commit

--- main.js
+++ main.js
@@ -25,12 +25,61 @@
 //   console.log(pais.innerHTML);
 // }
 
-let titulo = document.getElementById("parrafo1");
+// let titulo = document.getElementById("parrafo1");
 
-// console.log(titulo.innerText);
+// // console.log(titulo.innerText);
 
-// console.log(titulo.innerText);
+// // console.log(titulo.innerText);
 
-const nombreUsuario = prompt("bienvenido ingrese su nombre de usuario");
+// const nombreUsuario = prompt("bienvenido ingrese su nombre de usuario");
 
-titulo.innerText = "Hola " + nombreUsuario + "!";
+// titulo.innerText = "Hola " + nombreUsuario + "!";
+
+//// creatElement /////
+
+// let parrafo = document.createElement("p");
+
+// parrafo.innerHTML = "<h2>Hola Coder!!</h2>";
+
+// document.body.append(parrafo);
+
+///// remove //////
+
+// let parrafo = document.getElementById("parrafo1");
+// parrafo.remove();
+
+//// ejemplo aplicado /////
+
+// let contenedor = document.getElementById("personas");
+
+// let personas = ["Carlos", "Ana", "Jorge", "Pablo"];
+
+// for (const persona of personas) {
+//   let li = document.createElement("li");
+
+//   li.innerHTML = persona;
+
+//   contenedor.appendChild(li);
+// }
+
+////    CREANDO ELEMENTOS DESDE OBJETOS ////
+
+const productos = [
+  { id: 1, nombre: "Arroz", precio: 125 },
+  { id: 2, nombre: "Fideos", precio: 70 },
+  { id: 3, nombre: "Pan", precio: 50 },
+  { id: 4, nombre: "Flan", precio: 75 },
+];
+
+for (const producto of productos) {
+  let contenedor = document.createElement("div");
+
+  contenedor.innerHTML = `<div class="container">
+                            <h3>ID: ${producto.id}</h3>
+                            <p>Producto: ${producto.nombre}</p>
+                            <b>$${producto.precio}</b>
+                            </div>
+                            `;
+
+  document.body.appendChild(contenedor);
+}
